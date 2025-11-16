2e007111647431ce5bf783c4cc69a265a13f50fc
jetpax <jetpax@users.noreply.github.com>
2025-11-15 17:27:35 -0800

    Fix auto_restart reverted to True in v0.5.8 (v0.5.9)
    - Somehow auto_restart=False was changed back to True in v0.5.8
    - Changed both instances back to auto_restart=False
    - This parameter is not supported by the MicroPython CAN module

--- Extensions/OpenInverter/OpenInverter.js
+++ Extensions/OpenInverter/OpenInverter.js
@@ -2,7 +2,7 @@
 // {
 //   "name": "OpenInverter",
 //   "id": "openinverter",
-//   "version": [0, 5, 8],
+//   "version": [0, 5, 9],
 //   "author": "JetPax",
 //   "description": "OpenInverter debug and configuration tool for motor control parameters, spot values, CAN mapping, and live plotting",
 //   "icon": "sliders",
